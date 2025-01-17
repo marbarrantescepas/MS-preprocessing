7e8d807a848f2a1fc176ec03d2339ada3241f7fb
Mar Barrantes Cepas <mar.bc3@gmail.com>
2025-01-17 14:40:25 +0100

    Update run_fsqc.sh

--- run_fsqc.sh
+++ run_fsqc.sh
@@ -11,15 +11,25 @@
 #SBATCH --array=1-435%20		# first-last%parallel
 
 #======================================================================
-#                      QC PROGRAMS FREESURFER DATA
+#              VISUAL QUALITY CHECK FREESURFER DATA
 #======================================================================
 
 #@author: mar barrantes cepas
 #@email: m.barrantescepas@amsterdamumc.nl
-#updated: 12 Febuary 2024, works 
+#updated: 12 Febuary 2024, works
+#todo: change fsqc locally to luna-server
+
+#Description
+# Input: Folder includying subjects folders with FreeSurfer output
+# Output: Folder with CSV, screenshoots, HTML, and log files
+# Run: Captures different screenshots from the brain + surface 
+# segmentation from FreeSurfer and combines them into a HTML file. 
+# It also provides a CSV file with some parameters and log file.
+# See more information in https://github.com/Deep-MI/fsqc
 
 # Requirements: 
-# Please install the folowing tool https://github.com/Deep-MI/fsqc
+# Please install the fsqc tool https://github.com/Deep-MI/fsqc
+# If not already install in your system.
 
 #Please change the following things:
 # - array: change according the number of participants study
@@ -54,14 +64,11 @@ for sessdir in ${subject_dir}/*; do
     sess=${sessdir##/*/}
 
     if [ ! -d ${output}/${subjid} ]; then 
-
         /home/anw/mbarrantescepas/apps/fsqc/fsqc/run_fsqc --subjects_dir ${subject_dir} --output_dir ${output}/${subjid} \
         --screenshots_overlay none --screenshots-html --screenshots_layout 15 3 \
         --screenshots_views  x=-65 y=-65 z=-40 x=-60 y=-60 z=-35 x=-50 y=-50 z=-30 x=-40 y=-40 z=-40 x=-30 y=-30 z=-25 x=-20 y=-20 z=-20 \
         x=-10 y=-10 z=-10 x=0 y=0 z=0 x=10 y=10 z=10 x=20 y=20 z=20 x=30 y=30 z=30 x=40 y=40 z=35 x=50 y=50 z=40 x=50 y=50 z=45 x=60 y=60 z=50
-
     fi 
-
 done
 conda deactivate 
 
@@ -70,9 +77,6 @@ conda deactivate
 #---------------------------------------------------------------------
 
 #Esteban O, Birman D, Schaer M, Koyejo OO, Poldrack RA, Gorgolewski KJ; 2017; MRIQC: Advancing the Automatic Prediction of Image Quality in MRI from Unseen Sites; PLOS ONE 12(9):e0184661; doi:10.1371/journal.pone.0184661.
-
 #Wachinger C, Golland P, Kremen W, Fischl B, Reuter M; 2015; BrainPrint: a Discriminative Characterization of Brain Morphology; Neuroimage: 109, 232-248; doi:10.1016/j.neuroimage.2015.01.032.
-
 #Reuter M, Wolter FE, Shenton M, Niethammer M; 2009; Laplace-Beltrami Eigenvalues and Topological Features of Eigenfunctions for Statistical Shape Analysis; Computer-Aided Design: 41, 739-755; doi:10.1016/j.cad.2009.02.007.
-
 #Potvin O, Mouiha A, Dieumegarde L, Duchesne S, & Alzheimer's Disease Neuroimaging Initiative; 2016; Normative data for subcortical regional volumes over the lifetime of the adult human brain; Neuroimage: 137, 9-20; doi.org/10.1016/j.neuroimage.2016.05.016
