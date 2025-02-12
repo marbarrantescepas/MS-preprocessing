76aa0e186da0974ff7a938d5c9c592a7042d43f1
Mar Barrantes Cepas <mar.bc3@gmail.com>
2025-02-12 11:23:36 +0100

    Update 01_run_preproc-nicms_v2025.01.sh

--- code/nicms/01_run_preproc-nicms_v2025.01.sh
+++ code/nicms/01_run_preproc-nicms_v2025.01.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Below block for SLURM users. If you are not a SLURM user, remove this block 
+#and change below references to SLURM for parallel processing. 
 #SBATCH --job-name=preproc       	#a name for your job
 #SBATCH --mem=6G                    	#max memory per node
 #SBATCH --partition=luna-cpu-short    	#using luna short queue
